b69b874b5fe8048cc05abf0ecb4ef2de6f473057
Taras <shaprantaras2000@gmail.com>
2021-10-24 17:15:37 +0300

    fix

--- cron/index.js
+++ cron/index.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const removeOldTokens = require('./remove-old-token.job');
 
 module.exports = () => {
-    cron.schedule('2 0 * */1 *', async () => {
+    cron.schedule('0 0 * */1 *', async () => {
         await removeOldTokens();
     });
 };
